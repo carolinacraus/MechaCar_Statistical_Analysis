a6b51ab352696df4a7b1ed67983f859c362ae00a
carolinacraus <carocraus@gmail.com>
2020-11-08 20:29:59 -0600

    Add MechaCar_mpg.RScript

---
+++ R_Analysis/MechaCarChallenge.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/caroc/Desktop/R_Analysis/MechaCar_Statistical_Analysis/R_Analysis")
+
+library(tidyverse)
+
+MechaCar_mpg <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
